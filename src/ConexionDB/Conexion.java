f75592ca7919896eeb5cb9377ac9514307dec387
fragata <fragata@INNOVACION04>
2018-01-24 12:07:27 -0500

    sexta subida

--- src/ConexionDB/Conexion.java
+++ src/ConexionDB/Conexion.java
@@ -16,18 +16,19 @@ import java.util.logging.Logger;
  * @author fragata
  */
 public class Conexion {
-    private static final String USER="root";
-    private static final String PASSWORD="";
-    private static final String DRIVER="com.mysql.jdbc.Driver";
-    private static final String URL="jdbc:mysql://localhost:3306/serviciosem";
+
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/serviciosem";
     private Connection conexion;
 
     public Conexion() {
         crearConexion();
     }
 
-    public Connection crearConexion(){
-        
+    public Connection crearConexion() {
+
         try {
             Class.forName(DRIVER);
             conexion = DriverManager.getConnection(URL, USER, PASSWORD);
@@ -38,14 +39,43 @@ public class Conexion {
         }
         return conexion;
     }
-        private boolean confirmarTrasaccion(){
-            try{
-                conexion.commit();
-            } catch (SQLException ex){
-                Logger.getLogger(Conexion.class.getName()).log(record);
-            }
-            
+
+    private boolean confirmarTransaccion() {
+        try {
+            conexion.commit();
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
         }
-    
-    
+        return true;
+    }
+
+    private boolean reservarTransaccion() {
+        try {
+            conexion.rollback();
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+        return true;
+    }
+
+    public boolean puntoRestauracion() {
+        try {
+            conexion.setSavepoint();
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+        return true;
+    }
+
+    public boolean realizarTransaccion() {
+        if (confirmarTransaccion()) {
+            return true;
+        } else if (reservarTransaccion()) {
+            return false;
+        }
+        return false;
+    }
 }
