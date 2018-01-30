4f813b966e644da421553f136488b7f7ff6c91f1
123 <123@CAPACITACION>
2018-01-30 08:06:52 -0500

    subir 7

--- src/Modelo/DTO/RepLegal.java
+++ src/Modelo/DTO/RepLegal.java
@@ -11,7 +11,51 @@ package Modelo.DTO;
  */
 public class RepLegal {
     private Persona represenate;
+    private int numIdenRep;
+    private String nombreR;
+    private String apellidoR;
 
+    public RepLegal(Persona represenate, int numIdenRep, String nombreR, String apellidoR) {
+        this.represenate = represenate;
+        this.numIdenRep = numIdenRep;
+        this.nombreR = nombreR;
+        this.apellidoR = apellidoR;
+    }
+
+    public String getApellidoR() {
+        return apellidoR;
+    }
+
+    public String getNombreR() {
+        return nombreR;
+    }
+
+    public int getNumIdenRep() {
+        return numIdenRep;
+    }
+
+    public Persona getRepresenate() {
+        return represenate;
+    }
+
+    public void setApellidoR(String apellidoR) {
+        this.apellidoR = apellidoR;
+    }
+
+    public void setNombreR(String nombreR) {
+        this.nombreR = nombreR;
+    }
+
+    public void setRepresenate(Persona represenate) {
+        this.represenate = represenate;
+    }
+
+    public void setNumIdenRep(int numIdenRep) {
+        this.numIdenRep = numIdenRep;
+    }
+    
+    
+    
     public RepLegal() {
     }
 
