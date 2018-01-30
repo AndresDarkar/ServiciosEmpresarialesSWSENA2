3a7264613f99c093d0e2d437e9cabcbfae8f8061
123 <123@CAPACITACION>
2018-01-30 10:45:30 -0500

    ENVIO 11 :v

--- src/Modelo/DTO/Facturas.java
+++ src/Modelo/DTO/Facturas.java
@@ -16,6 +16,76 @@ public class Facturas {
     private Double precioEnvio;
     private String EnvioDesde;
     private String EnvioA;
+    private int Descuento;
+
+    public Facturas() {
+    }
+
+    public Facturas(int numfact, String Cliente, int numpaquetes, Double precioEnvio, String EnvioDesde, String EnvioA, int Descuento) {
+        this.numfact = numfact;
+        this.Cliente = Cliente;
+        this.numpaquetes = numpaquetes;
+        this.precioEnvio = precioEnvio;
+        this.EnvioDesde = EnvioDesde;
+        this.EnvioA = EnvioA;
+        this.Descuento = Descuento;
+    }
+
+    public String getCliente() {
+        return Cliente;
+    }
+
+    public int getDescuento() {
+        return Descuento;
+    }
+
+    public String getEnvioA() {
+        return EnvioA;
+    }
+
+    public String getEnvioDesde() {
+        return EnvioDesde;
+    }
+
+    public int getNumfact() {
+        return numfact;
+    }
+
+    public Double getPrecioEnvio() {
+        return precioEnvio;
+    }
+
+    public int getNumpaquetes() {
+        return numpaquetes;
+    }
+
+    public void setCliente(String Cliente) {
+        this.Cliente = Cliente;
+    }
+
+    public void setDescuento(int Descuento) {
+        this.Descuento = Descuento;
+    }
+
+    public void setEnvioA(String EnvioA) {
+        this.EnvioA = EnvioA;
+    }
+
+    public void setEnvioDesde(String EnvioDesde) {
+        this.EnvioDesde = EnvioDesde;
+    }
+
+    public void setNumfact(int numfact) {
+        this.numfact = numfact;
+    }
+
+    public void setNumpaquetes(int numpaquetes) {
+        this.numpaquetes = numpaquetes;
+    }
+
+    public void setPrecioEnvio(Double precioEnvio) {
+        this.precioEnvio = precioEnvio;
+    }
     
     
 }
--- src/Modelo/DTO/Pagos.java
+++ src/Modelo/DTO/Pagos.java
@@ -10,5 +10,62 @@ package Modelo.DTO;
  * @author 123
  */
 public class Pagos {
+    private int numPago;
+    private String fechaPa;
+    private String tipoPago;
+    private String Cliente;
+    private String montoPago;
+
+    public Pagos() {
+    }
+
+    public Pagos(int numPago, String fechaPa, String tipoPago, String Cliente, String montoPago) {
+        this.numPago = numPago;
+        this.fechaPa = fechaPa;
+        this.tipoPago = tipoPago;
+        this.Cliente = Cliente;
+        this.montoPago = montoPago;
+    }
+
+    public String getCliente() {
+        return Cliente;
+    }
+
+    public String getFechaPa() {
+        return fechaPa;
+    }
+
+    public String getMontoPago() {
+        return montoPago;
+    }
+
+    public int getNumPago() {
+        return numPago;
+    }
+
+    public String getTipoPago() {
+        return tipoPago;
+    }
+
+    public void setCliente(String Cliente) {
+        this.Cliente = Cliente;
+    }
+
+    public void setFechaPa(String fechaPa) {
+        this.fechaPa = fechaPa;
+    }
+
+    public void setMontoPago(String montoPago) {
+        this.montoPago = montoPago;
+    }
+
+    public void setNumPago(int numPago) {
+        this.numPago = numPago;
+    }
+
+    public void setTipoPago(String tipoPago) {
+        this.tipoPago = tipoPago;
+    }
+    
     
 }
